00cd33705132ade341a29fe61383187caeaac208
SURIYAPRAKASH S <hackersgod3105@gmail.com>
2026-02-03 01:02:04 +0530

    Refactor config to use Streamlit secrets

--- config/config.py
+++ config/config.py
@@ -1,12 +1,10 @@
-import os
+import streamlit as st
 
+LASTFM_API_KEY = st.secrets["LASTFM_API_KEY"]
+LASTFM_BASE_URL = "http://ws.audioscrobbler.com/2.0/"
 
-LASTFM_API_KEY = os.getenv("61099513ba8f84dc755a92ce55e26a11")
-LASTFM_BASE_URL = os.getenv( "http://ws.audioscrobbler.com/2.0/")
-
-YOUTUBE_API_KEY = os.getenv("AIzaSyB7XAE9TH1IIpA-NwkYl1ypfGC5ugl3vDk")
+YOUTUBE_API_KEY = st.secrets["YOUTUBE_API_KEY"]
 YOUTUBE_SEARCH_LIMIT = 10
-
 
 TOP_K_SONGS = 5
 LASTFM_FETCH_LIMIT = 50
